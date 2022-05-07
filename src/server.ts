89dd860bee3af88063d565253431625871af3cae
luiizsilverio <luiiz.silverio@gmail.com>
2022-05-07 10:57:03 -0300

    set PORT

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('NLW Server rodando na porta 3333')
 })
